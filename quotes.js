8f067b0d5fa131a39327987da10fecfde8e5ef4c
Irina Shakova <shakova.irina@gmail.com>
2025-09-30 08:42:21 +0300

    Spliting app into the  JS modules

---
+++ quotes.js
@@ -0,0 +1,56 @@
+const quotes = [
+  {
+    quote: "The only way to do great work is to love what you do",
+    author: "Steve Jobs",
+  },
+
+  {
+    quote: "Innovation distinguishes between a leader and a follower",
+    author: "Steve Jobs",
+  },
+
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: "Steve Jobs",
+  },
+
+  {
+    quote: "Strive not be a success, but rather to be of value",
+    author: "Albert Einstein",
+  },
+
+  {
+    quote: "The only thing We Have to fear is fear itself",
+    author: "Franklin  D. Roosevelt",
+  },
+
+  {
+    quote: "Life is what happens when you are making other plans",
+    author: "John Lennon",
+  },
+
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts",
+    author: "Winston S. Churchill",
+  },
+
+  {
+    quote:
+      "The greatest glory in living is not in falling, but in rising every time we fall",
+    author: "Nelson Mandela",
+  },
+
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams",
+    author: "Eleanor Roosevelt",
+  },
+
+  {
+    quote: "You miss 100 of the shots you don't take",
+    author: "Wayne Gretzky",
+  },
+];
+
+export default quotes;
