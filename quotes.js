5915eb62f4fb16c284f926fbbae1ca7ddab1e54f
Irina Shakova <shakova.irina@gmail.com>
2025-10-01 07:40:19 +0300

    Show favorite quotes below current quote

--- quotes.js
+++ quotes.js
@@ -2,54 +2,64 @@ const quotes = [
   {
     quote: "The only way to do great work is to love what you do",
     author: "Steve Jobs",
+    isFavorite: false,
   },
 
   {
     quote: "Innovation distinguishes between a leader and a follower",
     author: "Steve Jobs",
+    isFavorite: false,
   },
 
   {
     quote: "Your time is limited, so don't waste it living someone else's life",
     author: "Steve Jobs",
+    isFavorite: false,
   },
 
   {
     quote: "Strive not be a success, but rather to be of value",
     author: "Albert Einstein",
+    isFavorite: false,
   },
 
   {
     quote: "The only thing We Have to fear is fear itself",
     author: "Franklin  D. Roosevelt",
+    isFavorite: false,
   },
 
   {
     quote: "Life is what happens when you are making other plans",
     author: "John Lennon",
+    isFavorite: false,
   },
 
   {
     quote:
       "Success is not final, failure is not fatal: it is the courage to continue that counts",
     author: "Winston S. Churchill",
+    isFavorite: false,
   },
 
   {
     quote:
       "The greatest glory in living is not in falling, but in rising every time we fall",
     author: "Nelson Mandela",
+    isFavorite: false,
   },
 
   {
     quote:
       "The future belongs to those who believe in the beauty of their dreams",
     author: "Eleanor Roosevelt",
+    isFavorite: false,
   },
 
   {
     quote: "You miss 100 of the shots you don't take",
     author: "Wayne Gretzky",
+    isFavorite: false,
   },
 ];
 
