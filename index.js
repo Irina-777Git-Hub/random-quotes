a5ae60e4e5097724d341d4b2e9305dba9aa7e9d0
Irina Shakova <shakova.irina@gmail.com>
2025-10-24 18:25:24 +0300

    Favorites button icon and refactoring

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import quotes from "./quotes.js";
+import quotes from "./src/quotes.js";
+import {
+  hideFavoriteCard,
+  showFavoriteCard,
+  toggleFavoriteIcon,
+} from "./src/favoritesHandler.js";
 
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
@@ -16,9 +21,7 @@ function generateRandomQuote() {
 
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
-  toggleFavoriteBtn.textContent = randomQuote.isFavorite
-    ? "Remove from favorites"
-    : "Add to favorites";
+  toggleFavoriteIcon(randomQuote.isFavorite, toggleFavoriteBtn);
 
   toggleFavoriteBtn.style.display = "inline-block";
 }
@@ -26,29 +29,20 @@ function generateRandomQuote() {
 function toggleFavorite() {
   const currentQuote = quotes[currentQuoteIndex];
   currentQuote.isFavorite = !currentQuote.isFavorite;
-
-  toggleFavoriteBtn.textContent = currentQuote.isFavorite
-    ? "Remove from favorites"
-    : "Add to favorites";
+  toggleFavoriteIcon(currentQuote.isFavorite, toggleFavoriteBtn);
 
   if (currentQuote.isFavorite) {
-    const favoriteCard = document.createElement("div");
-    favoriteCard.classList.add("favorite-card");
-    favoriteCard.innerHTML = `<p>${currentQuote.quote}</p>  
-      <p class="author">${currentQuote.author}</p>`;
-    favoritesContainer.appendChild(favoriteCard);
+    showFavoriteCard(
+      currentQuote.quote,
+      currentQuote.author,
+      favoritesContainer
+    );
   } else {
-    // Remove favorite card if quote is unfavorited
-    const favoriteCards = document.querySelectorAll(".favorite-card");
-    favoriteCards.forEach((card) => {
-      if (card.textContent.includes(currentQuote.quote)) {
-        card.remove();
-      }
-    });
+    hideFavoriteCard(currentQuote.quote);
   }
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
 toggleFavoriteBtn.addEventListener("click", toggleFavorite);
 
-// generateRandomQuote();
+generateRandomQuote();
